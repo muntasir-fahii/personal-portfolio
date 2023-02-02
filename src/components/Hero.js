25feef8f27e1d3bc2676c7a2610260df32d96f94
muntasir-fahim <fa3877957@gmail.com>
2023-02-03 00:13:41 +0600

    headline added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,12 +12,12 @@ const Hero = () => {
   useHoverEffect(heroImageRef, data.img1, data.img2);
 
   return (
-    <div className="hero container mx-auto mt-20 ">
+    <div className="hero container mx-auto mt-20 flex justify-center items-center overflow-hidden">
       <div className="hero-img" ref={heroImageRef}></div>
-      <div className="shutter">
+      <div className="shutter shutter-left overflow-hidden">
         <h1>Front-End</h1>
       </div>
-      <div className="shutter">
+      <div className="shutter shutter-right overflow-hidden">
         <h1>Developer</h1>
       </div>
       <div className="circle-left"></div>
