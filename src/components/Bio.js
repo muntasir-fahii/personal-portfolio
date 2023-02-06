defe7f6cdd937276bad0e3dba1eac7f6f52d641a
muntasir-fahim <fa3877957@gmail.com>
2023-02-06 23:14:01 +0600

    hero section finished

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,15 @@
 const Bio = () => {
-  return <div>Bio</div>;
+  return (
+    <div className="bio mt-20 container mx-auto overflow-hidden">
+      <p>
+        Hi, I am a Experienced Front-End developer with a passion for building
+        dynamic and responsive web applications. Proficient in JavaScript,
+        React.js, Redux and Redux Toolkit. Committed to delivering clean,
+        efficient and high-quality code to meet project requirements and exceed
+        client expectations.
+      </p>
+    </div>
+  );
 };
 
 export default Bio;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import Hero from "./Hero";
+import Bio from "./Bio";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Bio />
     </div>
   );
 };
