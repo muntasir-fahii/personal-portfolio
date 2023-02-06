5399a67d05d713f69083442bc20e3415e52d90e3
muntasir-fahim <fa3877957@gmail.com>
2023-02-06 23:03:20 +0600

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,49 @@ import { HashLink } from "react-router-hash-link";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <HashLink smooth to="#home"></HashLink>
+    <nav className="navbar flex justify-between container mx-auto mt-20 uppercase">
+      <div>
+        <HashLink smooth to="#home" className="link-item">
+          Muntasir Fahim
+        </HashLink>
       </div>
+      <ul className="links flex flex-col gap-3">
+        <li>
+          <HashLink smoooth to="home" className="link-item">
+            Home
+          </HashLink>
+        </li>
+        <li>
+          <HashLink smoooth to="projects" className="link-item">
+            Projects
+          </HashLink>
+        </li>
+        <li>
+          <HashLink smoooth to="skills" className="link-item">
+            Skills
+          </HashLink>
+        </li>
+        <li>
+          <HashLink smoooth to="about" className="link-item">
+            About
+          </HashLink>
+        </li>
+        <li>
+          <HashLink smoooth to="contact" className="link-item">
+            Contact
+          </HashLink>
+        </li>
+        <li>
+          <a
+            href="https://www.google.com"
+            target="_balnk"
+            rel="noreferrer"
+            className="link-item"
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
